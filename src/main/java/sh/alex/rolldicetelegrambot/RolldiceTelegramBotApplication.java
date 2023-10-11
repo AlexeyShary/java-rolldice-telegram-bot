216fe52a144272270c6248aa44a8b811b3c2c5a7
Alexey Shary <badirbis@yandex.ru>
2023-10-11 09:52:27 +0300

    Initial commit

---
+++ src/main/java/sh/alex/rolldicetelegrambot/RolldiceTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package sh.alex.rolldicetelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolldiceTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolldiceTelegramBotApplication.class, args);
+	}
+
+}
